1b39cca2eaf01d84ebb2a361f5155bc86665bc1d
Pangwoo <gwangwoo25@gmail.com>
2023-04-07 19:33:17 -0700

    API working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Movies from "./routes/Movies";
+import Movie from "./routes/Movie";
 
-
-function App() {
-  return <div></div>;
+export default function App() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Movies />} />
+      <Route path="/movies/:id" element={<Movie />} />
+    </Routes>
+  </BrowserRouter>;
 }
 
-export default App;
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,3 +1,28 @@
-export function Movies() {
-    return <div></div>
-}
+import { gql, useApolloClient } from "@apollo/client";
+import { useEffect, useState } from "react";
+
+export default function Movies() {
+  const [movies, setMovies] = useState([]);
+  const client = useApolloClient();
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              title
+              id
+            }
+          }
+        `,
+      })
+      .then((results) => setMovies(results.data.allMovies));
+  }, [client]);
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+  );
+      }
